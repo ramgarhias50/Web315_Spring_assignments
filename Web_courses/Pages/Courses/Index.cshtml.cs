235fbb2201597449d08aefbc0f1a9486a0b7d0a4
Gursimran Singh Assi <gursimransinghassi@gmail.com>
2023-06-22 17:54:00 -0400

    All done

--- Web_courses/Pages/Courses/Index.cshtml.cs
+++ Web_courses/Pages/Courses/Index.cshtml.cs
@@ -21,17 +21,29 @@ namespace Web_courses.Pages_Courses
         public IList<Course> Course { get; set; }
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string coursescategory { get; set; }
+        public SelectList coursesCategory { set; get; }
 
 
 
         public async Task OnGetAsync()
         {
+            IQueryable<string> courseQuery = from m in _context.Course orderby m.coursesCategory select m.coursesCategory;
+
+
             var course = from m in _context.Courses
                          select m;
             if (!string.IsNullOrEmpty(SearchString))
             {
                 course = course.Where(s => s.Title.Contains(SearchString));
             }
+
+            if (!string.IsNullOrEmpty(coursescategory))
+            {
+                course = course.Where(x => x.coursesCategory == coursescategory);
+            }
+            coursesCategory = new SelectList(await courseQuery.Distinct().ToListAsync());
             Course = await course.ToListAsync();
 
 
