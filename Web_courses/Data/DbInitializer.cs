33cc0b54de66e9ba5f39e19ec2f1ad0a82e75f76
Gursimran Singh Assi <gursimransinghassi@gmail.com>
2023-06-22 16:14:23 -0400

    Changes some name of properties

--- Web_courses/Data/DbInitializer.cs
+++ Web_courses/Data/DbInitializer.cs
@@ -12,11 +12,11 @@ namespace Web_courses
                 return;
             }
             var course = new Course[]{
-                new Course{CourseID=1,Title="HTML",Credits=4,LaunchDate=DateTime.Parse("2023-09-01"),ratings=2,reviews="Very Good",Instructor="Sahil", coursesCategory="Web"},
-                new Course{CourseID=2,Title="CSS",Credits=2,LaunchDate=DateTime.Parse("2023-09-01"),ratings=2,reviews="Very Good",Instructor="Sahil", coursesCategory="Web"},
-                new Course{CourseID=3,Title="JavaScript",Credits=5,LaunchDate=DateTime.Parse("2023-09-01"),ratings=2,reviews="Very Good",Instructor="Sahil", coursesCategory="Web"},
-                new Course{CourseID=4,Title="Asp.NET",Credits=5,LaunchDate=DateTime.Parse("2023-09-01"),ratings=2,reviews="Very Good",Instructor="Sahil", coursesCategory="Web"},
-                new Course{CourseID=5,Title="PHP",Credits=5,LaunchDate=DateTime.Parse("2023-09-01"),ratings=2,reviews="Very Good",Instructor="Sahil",coursesCategory="dd"},
+                new Course{CourseID=1,Title="HTML",Credits=4,LaunchDate=DateTime.Parse("2023-09-01"),Ratings=2,reviews="Very Good",Instructor="Sahil", coursesCategory="Web"},
+                new Course{CourseID=2,Title="CSS",Credits=2,LaunchDate=DateTime.Parse("2023-09-01"),Ratings=2,reviews="Very Good",Instructor="Sahil", coursesCategory="Web"},
+                new Course{CourseID=3,Title="JavaScript",Credits=5,LaunchDate=DateTime.Parse("2023-09-01"),Ratings=2,reviews="Very Good",Instructor="Sahil", coursesCategory="Web"},
+                new Course{CourseID=4,Title="Asp.NET",Credits=5,LaunchDate=DateTime.Parse("2023-09-01"),Ratings=2,reviews="Very Good",Instructor="Sahil", coursesCategory="Web"},
+                new Course{CourseID=5,Title="PHP",Credits=5,LaunchDate=DateTime.Parse("2023-09-01"),Ratings=2,reviews="Very Good",Instructor="Sahil",coursesCategory="dd"},
             };
             context.Course.AddRange(course);
             context.SaveChanges();
--- Web_courses/Models/Web_Courses.cs
+++ Web_courses/Models/Web_Courses.cs
@@ -7,14 +7,46 @@ namespace Web_courses
     public class Course
     {
         public int CourseID { get; set; }
+        [Required]
+        [StringLength(32, MinimumLength = 4)]
+        [RegularExpression("^[A-Z][A-Za-z ]*$")]
         public string Title { get; set; }
+
+
+        [Required(ErrorMessage = "Don't Empty the fields")]
+        [StringLength(1, MinimumLength = 1)]
+        [RegularExpression("[1-5]+")]
         public int Credits { get; set; }
-        public DateTime LaunchDate { get; set; }
+
+        [Required]
         [DataType(DataType.Date)]
+        public DateTime LaunchDate { get; set; }
+
+
+        [Required]
+        [StringLength(32, MinimumLength = 4)]
+        [RegularExpression("[A-Za-z]+")]
         public string Instructor { get; set; }
+
+
+        [Required]
+        [StringLength(1, MinimumLength = 1)]
+        [RegularExpression("^[A-Z][A-Za-z ]*$")]
         public string coursesCategory { get; set; }
-        public int ratings {get; set;}
-        public string reviews {get; set;}
+
+
+        [Required]
+        [StringLength(1, MinimumLength = 1)]
+        [RegularExpression("[1-5]")]
+        public int Ratings { get; set; }
+
+
+        [Required]
+        [StringLength(32, MinimumLength = 4)]
+        [RegularExpression("[A-Za-z]+")]
+        public string reviews { get; set; }
+
+
 
 
 
